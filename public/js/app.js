b29f2238f9c7114fea3f49b80cdb54253b39889e
catalin <ccretoiu2@lenovo.com>
2021-02-20 23:00:37 +0200

    heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,12 +12,13 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
 
+
     const location = search.value
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' +  location).then((response) => {
+    fetch('/weather?address=' +  location).then((response) => {
     response.json().then((data)=>{
         if (data.error) {
            return  messageOne.textContent = data.error
